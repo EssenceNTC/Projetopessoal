62e13450887dc2320b634f322577c6eee934b552
EssenceNTC <rodrigoawscloud@outlook.com>
2026-01-15 00:54:54 +0000

    parte com JS

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function ativaletra (elemento){
+const arrtexto = elemento.innerHTML.split('')
+elemento.innerHTML = '';
+arrtexto.array.forEach(letra, i)=> 
+setTimeout(()=>{
+    elemento.innerHTML += letra;
+{, (75 * i)   
+})
+}
+
+const titulo = document.querySelector('.digitanto');
+ativaletra(titulo);
